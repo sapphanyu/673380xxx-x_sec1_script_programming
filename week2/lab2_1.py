7acc7b6c1cefd779d9cb8d1b37ff659f19d2d72c
sapphanyu <sapphanyu.k@kkumail.com>
2026-07-03 22:37:55 +0700

    แก้ไขคืน กู้คืนไฟล์

---
+++ week2/lab2_1.py
@@ -0,0 +1,16 @@
+#แก้ไขโค้ดนี้ให้ถูกต้อง
+
+num = int(input("Enter a number: ") )
+if num < 0:
+    text = f"{num} is negative"
+elif num > 0:
+    text = f"{num} is positive"
+else:
+    text = f"{num} is zero"
+
+if num == 0:
+    print("1")
+elif num % 2 == 0:
+    print("2")
+else:
+    print("3")
